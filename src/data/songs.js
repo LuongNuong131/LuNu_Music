40ade90a0edad8bbdbe4477c5635638455c0f05f
LuNu <tranquangluong06@gmail.com>
2026-07-04 02:04:58 +0700

    first commit

--- src/data/songs.js
+++ src/data/songs.js
@@ -9,63 +9,63 @@ export const songs = [
     id: 1,
     title: 'Quan Hệ Rộng',
     artist: 'Donald Gold',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/quan_he_rong.mp3'
   },
   {
     id: 2,
     title: 'Ngáo Ngơ Hết Ngày',
     artist: 'Donald Gold',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/ngao_ngo_het_ngay.mp3'
   },
   {
     id: 3,
     title: 'Đỉnh (Sao Hạng A Dissing)',
     artist: 'Donald Gold',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/dinh.mp3'
   },
   {
     id: 4,
     title: 'Trăng Hoa Mây Mưa',
     artist: 'Donald Gold',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/trang_hoa_may_mua.mp3'
   },
   {
     id: 5,
     title: 'OBGTLH',
     artist: 'Donald Gold x Lil Shady',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/obgtlh.mp3'
   },
   {
     id: 6,
     title: 'Lái Máy Bay',
     artist: 'Donald Gold',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/lai_may_bay.mp3'
   },
   {
     id: 7,
     title: 'Đổi Tư Thế',
     artist: 'Donald Gold x Andree Right Hand',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/doi_tu_the.mp3'
   },
   {
     id: 8,
     title: 'ADAMN',
     artist: 'Donald Gold',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/adamn.mp3'
   },
   {
     id: 9,
     title: 'BCDBL',
     artist: 'Donald Gold',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/bcdbl.mp3'
   },
 
@@ -76,28 +76,28 @@ export const songs = [
     id: 10,
     title: 'Chơi Như Tụi Mỹ',
     artist: 'Andree Right Hand',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/choi_nhu_tui_my.mp3'
   },
   {
     id: 11,
     title: 'Công Ty 4',
     artist: 'Andree Right Hand ft. Dangrangto, TeuYungBoy, WOKEUP',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/cong_ty_4.mp3'
   },
   {
     id: 12,
     title: 'Em Iu (Cleaned Version)',
     artist: 'Andree Right Hand ft. Wxrdie, Bình Gold, 2pillz',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/em_iu.mp3'
   },
   {
     id: 13,
     title: 'Anh Tên Là',
     artist: 'Bằng Cổ Tay ft. Nhims, Anh Nguyễn',
-    cover: '../src/assets/images/ChoCiu.jpg',
+    cover: '/images/ChoCiu.jpg',
     url: '/music/anh_ten_la.mp3'
   }
 ];
